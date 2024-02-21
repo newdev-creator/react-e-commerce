c799d8ae2934843dbeb510a2494459a5509fdaf3
Julien Freisa <81742220+newdev-creator@users.noreply.github.com>
2024-02-21 10:57:20 +0100

    FEAT: cart's content

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -17,24 +17,24 @@ export default function ProductsList() {
       <h1 className="text-slate-100 text-2xl mb-6">Here are our products</h1>
       <ul className="grid min-[500px]:grid-cols-2 md:grid-cols-3 gap-4">
         {products.items &&
-          products.items.map((item) => (
-            <li key={item.id} className="p-4 bg-slate-200 rounded">
+          products.items.map((el) => (
+            <li key={el.id} className="p-4 bg-slate-200 rounded">
               <img
                 className="mb-4"
-                src={`/images/${item.img}.png`}
-                alt={`${item.title} picture`}
+                src={`/images/${el.img}.png`}
+                alt={`${el.title} picture`}
               />
               <div className="flex justify-between items-center mb-6">
-                <p className="text-slate-700 text-lg">{item.title}</p>
-                <p className="text-slate-900 font-bold">{item.price}</p>
+                <p className="text-slate-700 text-lg">{el.title}</p>
+                <p className="text-slate-900 font-bold">{el.price}</p>
               </div>
               <button
-                onClick={() => dispatch(addOneToCart(item.id))}
+                onClick={() => dispatch(addOneToCart(el.id))}
                 className={`${
-                  item.picked ? "bg-green-700" : "bg-slate-600"
+                  el.picked ? "bg-green-700" : "bg-slate-600"
                 } w-full text-slate-100 px-2 inline-flex items-center justify-center rounded p-2 mr-2`}
               >
-                {item.picked ? "Item picked ✔️" : "Add to cart"}
+                {el.picked ? "Item picked ✔️" : "Add to cart"}
               </button>
             </li>
           ))}
